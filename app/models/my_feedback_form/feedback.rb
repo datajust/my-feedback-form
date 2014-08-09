d2d69c2cfa21b2da3381658f559fd3e3a39e83c3
Michael Riviera <mriviera@ri4a.com>
2014-08-09 21:57:46 +0200

    field?

--- app/models/my_feedback_form/feedback.rb
+++ app/models/my_feedback_form/feedback.rb
@@ -2,10 +2,10 @@ module MyFeedbackForm
   class Feedback
 #      include Mongoid::Document
 
-      field :user_id, String
-      field :kind, String
-      field :message, String
-      field :email, String
+#      field :user_id, String
+#      field :kind, String
+#      field :message, String
+#      field :email, String
 #    attr_accessible :message, :kind, :user_id
 
     validates_presence_of :email, :message, :kind, :on => :create
